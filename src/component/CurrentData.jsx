8512a9808e76f8f2bf1671e5244eae37ac7dc7af
N0rphel <tnorphel09@gmail.com>
2023-12-03 20:13:35 +0600

    firebase work

--- src/component/CurrentData.jsx
+++ src/component/CurrentData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import PlantSVG from "./svg/Plant";
 import Thermometer from "./svg/Thermometer";
@@ -6,24 +6,29 @@ import fetchSensorData from "../firebase/getService";
 
 function CurrentData() {
 
+  const [sensorData, setSensorData] =  useState({
+    Fan: "",
+    Temp: "",
+    pH: ""
+  });
+
   useEffect(() => {
     const handleSensorData = (data) => {
-      console.log('Retrieved Data: ', data);
+      setSensorData(data);
+      console.log('Retrieved Data: ', sensorData);
     }
 
     fetchSensorData('SensorData/1', handleSensorData);
-  }, [])
+  }, []);
 
   return (
     <div className="current-container">
       <div className="factor-container">
         <h1>Today's Record</h1>
         <ul className="factor">
-          <li>Temperature: 12</li>
-          <li>ph: 12</li>
-          <li>Humidity: 12</li>
-          <li>Mineral: 12</li>
-          <li>Fan: on</li>
+          <li>Temperature: {sensorData.Temp}</li>
+          <li>pH: {sensorData.pH}</li>
+          <li>Fan: {sensorData.Fan}</li>
         </ul>
       </div>
       <div className="image-container">
